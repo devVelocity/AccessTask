7853950fe1e4e07516bd714c7da69dcc494cdbef
88147999+exeProgram@users.noreply.github.com <88147999+exeProgram@users.noreply.github.com>
2022-04-10 20:30:53 +0100

    footer finished

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="w-full h-auto min-h-[30px] flex items-center justify-between p-4 bg-zinc-900 sticky mt-32">
-      <h2 class="text-white text-sm">Website made by Kieran P</h2>
+      <div class="">
+        <h2 class="text-white text-sm">Website made by Kieran P - Made for Access Task B</h2>
+        <a href="https://github.com/devVelocity/AccessTask" class="text-yellow-700 hover:text-yellow-500 duration-100 hover:underline text-left inline-block w-full text-sm">See Source Code on GitHub</a>
+      </div>
       <h1 style="font-family: 'Press Start 2P', cursive;" class="text-yellow-500 text-sm md:text-lg">Poke<span style="font-family:Avenir, Helvetica, Arial, sans-serif;" class="text-white md:text-sm ml-2 text-xs">API</span></h1>
   </div>
 </template>
